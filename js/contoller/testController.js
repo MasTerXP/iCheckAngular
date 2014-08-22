295cb1c29d713a47ab4916677f30dc9dd54ca678
Peerapong <peerapong@lannasoftworks.com>
2014-08-22 13:35:18 +0700

    New way use
    http://plnkr.co/edit/toxxrRTfmltyamtI7ZG6?p=preview

--- js/contoller/testController.js
+++ js/contoller/testController.js
@@ -1,6 +1,7 @@
 var app = angular.module("VideoResponse",[]);
 
 app.controller("submitTest",['$scope',function($scope){
+  $scope.radioValue = "test";
 	$scope.submit = function myfunction(){
 		console.log("test");
 		console.log($scope.txt);
@@ -9,33 +10,40 @@ app.controller("submitTest",['$scope',function($scope){
 }]);
 
 
-app.directive('iCheck', function($timeout, $parse) {
+app.directive('icheck',[ '$timeout', function($timeout, $parse) {
     return {
-      link: function($scope, element, $attrs) {
+      require:'ngModel',
+      link: function($scope, element, $attrs, ngModel) {
         return $timeout(function() {
-          var ngModelGetter, value;
+          /*var ngModelGetter, value;
           ngModelGetter = $parse($attrs['ngModel']);
           value = $parse($attrs['ngValue'])($scope);
-          console.log(element);
+          console.log(element);*/
+          var value = $attrs['value'];
+          $scope.$watch($attrs['ngModel'],function(newValue){
+            console.log("watch");
+            $(element).iCheck('update');
+          })
           return $(element).iCheck({
            /* checkboxClass: 'icheckbox_minimal',*/
             radioClass: 'iradio_minimal-red',
             checkboxClass: 'icheckbox_minimal-red',
             increaseArea: '20%'
           }).on('ifChanged', function(event) {
-          	console.log("ifChanged");
             if ($(element).attr('type') === 'checkbox' && $attrs['ngModel']) {
               $scope.$apply(function() {
-                return ngModelGetter.assign($scope, event.target.checked);
+                /*return ngModelGetter.assign($scope, event.target.checked);*/
+                return ngModel.$setViewValue(event.target.checked);
               });
             }
             if ($(element).attr('type') === 'radio' && $attrs['ngModel']) {
               return $scope.$apply(function() {
-                return ngModelGetter.assign($scope, value);
+                /*return ngModelGetter.assign($scope, value);*/
+                return ngModel.$setViewValue(value);
               });
             }
           });
         });
       }
     };
-  })
+  }])
